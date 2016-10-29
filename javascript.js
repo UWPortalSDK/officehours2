4c955ebfd7196a0e18b7a036c3878d56e8f3758d
bavaz <bevantvaz@yahoo.com>
2016-10-29 13:33:44 -0300

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -42,6 +42,8 @@ angular.module('portalApp')
 		}
 	];
     
+    $scope.subitems=[
+    
     
 	// Show main view in the first column as soon as controller loads
 	$scope.portalHelpers.showView('officehoursMain.html', 1);
