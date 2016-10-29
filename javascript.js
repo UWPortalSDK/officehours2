84d9f897da076fe92753ec9516905889d7a03117
meenadeschutter <mmdeschu@uwaterloo.ca>
2016-10-29 11:15:12 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -4,32 +4,32 @@ angular.module('portalApp')
 	// mock data
 	$scope.items = [
 		{
-			title:'Item 1',
+			title:'Course 1',
 			tags: ['tag A', 'tag B', 'tag C'],
 			details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 		},
 		{
-			title:'Item 2',
+			title:'Course 2',
 			tags: ['tag D', 'tag E', 'tag F'],
 			details: 'Mauris cursus, sapien et malesuada ultrices, purus sapien iaculis tellus, quis semper magna est at leo.'
 		},
 		{
-			title:'Item 3',
+			title:'Course 3',
 			tags: ['tag A', 'tag H'],
 			details: 'Donec id quam eu odio feugiat sagittis. Duis a tempus neque. Praesent elementum quis ante quis commodo. Sed tincidunt aliquet dolor sit amet laoreet. '
 		},
 		{
-			title:'Item 4',
+			title:'Course 4',
 			tags: ['tag I'],
 			details: 'Proin sem quam, rutrum id ante id, scelerisque tempor quam. Curabitur pharetra turpis at sem placerat, non vehicula ligula tincidunt.'
 		},
 		{
-			title:'Item 5',
+			title:'Course 5',
 			tags: ['tag C', 'tag K', 'tag B'],
 			details: 'Mauris nec ultricies metus. Cras et dictum justo. Nam a ullamcorper dolor. Cras fringilla metus vel facilisis vehicula.'
 		},
 		{
-			title:'Item 6',
+			title:'Course 6',
 			tags: ['tag A', 'tag B', 'tag C'],
 			details: 'Curabitur scelerisque lorem risus, in luctus orci hendrerit non. Praesent quis tellus dapibus dolor consectetur volutpat.'
 		}
